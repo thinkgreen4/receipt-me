9b5e5d22ca044ba86f6c5453db92f84b95731a56
thinkgreen4 <thinkgreen4@hotmail.com>
2018-10-05 11:07:05 -0600

    tutor

---
+++ client/src/components/Tesseract/Tesseract.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Tesseract from 'Tesseract.js';
+import API from '../../utils/api.js';
+
+class Tesseract extends Component {
+    state
+    Tesseract.recognize(myImage)
+    .then(function(result){
+        console.log(result)
+    });
+}
+
+export default Tesseract;
