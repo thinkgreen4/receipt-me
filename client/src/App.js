3b66f4f7be1a96e728f84e7aee1c845d292619c5
colin <colin.rhys.richards@gmail.com>
2018-10-07 17:51:00 -0600

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -9,36 +9,6 @@ import ImgRecognition from './components/Tesseract';
 import Tesseract from 'tesseract.js';
 import PortfolioContainer from "./components/PortfolioContainer";
 
-
-
-
-// class App extends Component {
-//   state = {
-//     openCameraState: false
-//   }
-
-//   ProcessOCR = (myImage) => {
-//     Tesseract.recognize(myImage)
-//  .then(function(result){
-//      console.log(result)
-//  });
-// }
-
-//   openCamera = (event) => {
-//     event.preventDefault()
-//     this.setState({
-//       openCameraState: true
-//     });
-//   }
-//   closeCamera = (event) => {
-//     event.preventDefault()
-//     this.setState({
-//       openCameraState: false
-//     });
-//   }
-
-
-
   const App = () => (
     <div className="wrapper">
       <div className="row main-row">
@@ -49,7 +19,7 @@ import PortfolioContainer from "./components/PortfolioContainer";
         </div>
         <div className="col-md-8">
           <div>
-          <div className="row">
+          <div className="row main-right">
           <div className="col-md-6"></div>
           <div className="col-md-6">
           <div className="btn-group btn-group-toggle" data-toggle="buttons">
@@ -61,20 +31,7 @@ import PortfolioContainer from "./components/PortfolioContainer";
         </div>
       </div>
     </div>
-        {/* <div>
-        <button onClick={this.openCamera}>Take Photo</button>
-        <button onClick={this.closeCamera}>Close</button>
-        <button onClick={this.processOCR}>Scan</button>
-          {this.state.openCameraState && <Camera processOCR={this.ProcessOCR}/>}
-        </div> */}
     </div>
-    {/* <div className="row">
-    <div className="col-md-3"></div>
-    <div className="col-md-6">
-    <Receipt></Receipt>
-    </div>
-    <div className="col-md-3"></div>
-    </div> */}
     </div>
     );
   
